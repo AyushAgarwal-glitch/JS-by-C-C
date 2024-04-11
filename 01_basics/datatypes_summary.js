59903b575ea6dd069d7a0c3c75b7be21c4279f53
AyushAgarwal-glitch <119740113+AyushAgarwal-glitch@users.noreply.github.com>
2024-04-11 07:57:07 +0000

    datatypes with memory

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -28,3 +28,30 @@ const myFunction = function(){
 }
 
 console.log(typeof bigNumber)
+
+// ***********************************************************
+//Stack memory(Primitive datatypes)(we get a copy of the data stored in stack memory)
+//Heap memory(Non-Primitive datatypes)(we get a refernec to the heap where the data is stored)
+
+//stack memmory
+let myYTName = "YT@yt.com"
+let anotherName = myYTName 
+console.log(anotherName)
+anotherName = "Chai Aur Code" 
+console.log(anotherName)
+console.log(myYTName)
+//in stack memory we get a copy of the data and hence when we retreat something from the stack memory we get a copy of that data
+//hence any changes made on a certain variable remain confined to only that variable and are not reflected everywhere
+
+//heap memory
+let userOne = {
+    email: "user@google.com",
+    upi: "34343@ytr"
+}
+let userTwo = userOne
+userTwo.email = "Newemail@google.com"
+console.log(userTwo.email)
+console.log(userOne.email) //userOne.email value is also changed bcoz in heap memory reference is passed hence it gives refeernce
+                           //to the original space where data was stored and changes its data also
+//userOne and userTwo both get the refernce of the same heap hence changes made on any one of them will be reflecteed on both
+//of them 
