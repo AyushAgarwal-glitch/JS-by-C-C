411510f4bc692b1e51f2abd37a37cdd8ed4f955d
AyushAgarwal-glitch <119740113+AyushAgarwal-glitch@users.noreply.github.com>
2024-04-18 12:57:09 +0000

    this keyword and arrow functions

--- 03_basics/03_arrow_function.js
+++ 03_basics/03_arrow_function.js
@@ -22,3 +22,26 @@ function chai () {
     console.log(this.username) // this gives undefined specifying that we cant use this inside functions and only inside objects
 }
 chai() 
+
+const chaii = () => {  //this is a arrow function
+    let username = "hitesh"
+    // console.log(this) //this gives empty {} 
+    console.log(this.username) //it still gives undefined
+}
+chaii()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+// console.log(addTwo(4,56))
+
+const addTwo = (num1, num2) => (num1 + num2) 
+//above is another method of arrow functions known as implicit return therefore we dont need to wrtiee the return keyword 
+//it accepts it as return only , the value after the arrows is returned 
+console.log(addTwo(4,3))
+
+//we can wrap num1+num2 in () also , if we wrap it in {} then we wil have to write the return keyword
+const addTw = (num1 , num2) => {username: "ayush"} ///this would give undefined
+console.log(addTw(3,2))
+const addt = (num1,num2) => ({username:"Ayush"}) //now this would print the object value bcoz it is wrapped in ()
+console.log(addt(5,4))
