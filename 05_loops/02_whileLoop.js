549f0909cc0ab9757b6c27f525471ce72313b4f9
AyushAgarwal-glitch <119740113+AyushAgarwal-glitch@users.noreply.github.com>
2024-04-23 18:03:44 +0000

    while loop

---
+++ 05_loops/02_whileLoop.js
@@ -0,0 +1,20 @@
+// while loops
+/*while (condition) {
+    
+}*/
+
+let i = 0 ;
+while(i <= 10){
+    console.log(i)
+    i += 2 ;
+}
+let myArr = ['f','b','s']
+let index = 0 ;
+while(index < myArr.length){
+    console.log(myArr[index++])
+}
+
+//do while loop// condition at end
+/*do {
+    
+} while (condition);*/
